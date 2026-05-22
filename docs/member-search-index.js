9cf90aa577bedeb17e82573fcc53a1488674f166
enchenkova52-dotcom <enchenkova52@yandex.ru>
2026-05-22 14:17:40 +0300

    Add all practices with Javadoc

---
+++ docs/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"<Unnamed>","c":"AllDataTypes","l":"AllDataTypes()","u":"%3Cinit%3E()","k":"3"},{"p":"<Unnamed>","c":"AllDataTypes","l":"main(String[])","u":"main(java.lang.String[])","k":"6"}];updateSearchResults();
